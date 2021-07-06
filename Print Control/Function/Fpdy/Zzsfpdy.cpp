f395c019c0c920e32c3084b54286aa2f3133a45d
Wangsl <wslperfect@163.com>
2021-07-06 16:07:51 +0800

    增加 完税凭证号

--- Print Control/Function/Fpdy/Zzsfpdy.cpp
+++ Print Control/Function/Fpdy/Zzsfpdy.cpp
@@ -773,18 +773,19 @@ CString CZzsfpdy::GenerateItemXml(ZZSFP_FPXX fpmx, FPDY fpdy)
 		xywhsf(fpmx.Xhdwsbh, 340, 800, 580, 50, LS_12, FT, AM_VCL);
 		xywhsf(xmDbjg, 920, 800, 200, 50, LS_9, FS, AM_VCR_S);
 		addxml("（代开单位）", xmDbjg);
+
+		xywhsf(fpmx.Xhdwyhzh, 340, 900, 580, 50, LS_9, FS, AM_VCL);
+		xywhsf(xmDbjg, 920, 900, 200, 50, LS_9, FS, AM_VCR_S);
+		addxml("（完税凭证号）", xmDbjg);
 	}
 	else
 	{
 		xywhsf(fpmx.Xhdwmc, 340, 750, 780, 50, LS_9, FS, AM_VCL);
 		xywhsf(fpmx.Xhdwsbh, 340, 800, 780, 50, LS_12, FT, AM_VCL);
+		xywhsf(fpmx.Xhdwyhzh, 340, 900, 780, 50, LS_9, FS, AM_VCL);
 	}
 
 	xywhsf(fpmx.Xhdwdzdh, 340, 850, 780, 50, LS_9, FS, AM_VCL);
-
-	xywhsf(fpmx.Xhdwyhzh, 340, 900, 780, 50, LS_9, FS, AM_VCL);
-	//xywhsf(xmDbjg, 920, 900, 200, 50, LS_9, FS, AM_VCR_S);
-	//addxml("（完税凭证号）", xmDbjg);
 
 	xywhsf(fpmx.skr, 200, 950, 330, 60, LS_10, FS, AM_VCL);
 	xywhsf(fpmx.fhr, 720, 950, 290, 60, LS_10, FS, AM_VCL);
